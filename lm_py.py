099f3174d1fbe8dc882d61cada444d574e3160a5
Jana Fisher <janafisher2331@pitzer-rw01.ten.osc.edu>
2024-07-07 21:48:03 -0400

    final commit

---
+++ lm_py.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Linear Model in Python
+
+# In[1]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import sys
+
+
+# # Load Dataset
+
+# In[ ]:
+
+
+df = pd.read_csv("regrex1.csv")
+
+
+# In[ ]:
+
+
+df.head()
+
+
+# # Create Scatter Plot
+
+# In[ ]:
+
+
+plt.scatter(df['x'],df['y'])
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+
+
+# # Create Linear Model of Data
+
+# In[ ]:
+
+
+model=LinearRegression()
+
+
+# In[ ]:
+
+
+x = np.array(df['x']).reshape(-1,1)
+y = np.array(df['y'])
+
+
+# In[ ]:
+
+
+model.fit(x,y)
+
+
+# In[ ]:
+
+
+r_sq = model.score(x,y)
+print(f"intercept: {model.intercept_}")
+print(f"slope: {model.coef_}")
+
+
+# In[ ]:
+
+
+y_pred = model.predict(x)
+
+
+# # Plot Linear Model
+
+# In[ ]:
+
+
+plt.scatter(x,y)
+plt.plot(x,y_pred)
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+
